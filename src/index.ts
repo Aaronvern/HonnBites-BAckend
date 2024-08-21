65af6670e1ae6d24581b12ada0dbb8c60068adde
Aaronvern <aaronvernekar@gmail.com>
2024-08-21 23:06:12 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { Request,Response } from "express";
 import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute"
 
 const port =7000
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
@@ -13,12 +14,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors())
-
-app.get("/test",async(req: Request , res: Response)=>{
-    res.json({
-        msg:"hello bro"
-    })
-})
+app.use("/api/my/user",MyUserRoute)
 
 app.listen(port,()=>{
     console.log("listening on port: "+port)
