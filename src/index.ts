a2b1679fa0dbd0de856969a193a2d03dc8cd4003
Aaronvern <aaronvernekar@gmail.com>
2024-09-16 15:30:11 +0530

    added search restaurant endpoint with pagination feature and Restaurant route

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,37 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import mongoose from "mongoose";
-import MyUserRoute from "./routes/MyUserRoute"
-import { v2 as cloudinary } from "cloudinary"
+import MyUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
 import MyRestaurantRoute from "./routes/MyRestaurantRoute";
+import RestaurantRoute from "./routes/RestaurantRoute";
 
-const port =7000
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
-    console.log("connected to mongodb")
-})
+const port = 7000;
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("connected to mongodb");
+});
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/server-check", (req: Request, res: Response) => {
+  res.send({ msg: "server is up and running" });
+});
 
-app.get("/server-check",(req:Request,res:Response)=>{
-    res.send({msg :"server is up and running"})
-})
+app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute); // typo fixed
+app.use("/api/restaurant", RestaurantRoute);
 
-app.use("/api/my/user",MyUserRoute)
-app.use("/api/my/restaurant",MyRestaurantRoute) // typo fixed
-
-app.listen(port,()=>{
-    console.log("listening on port: "+port)
-})
+app.listen(port, () => {
+  console.log("listening on port: " + port);
+});
